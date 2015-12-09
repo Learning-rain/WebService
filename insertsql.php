12f30613c934d080a0c72ecde44f836d9258e718
Sonia Martinez <soniamartinezctr@gmail.com>
2015-12-09 16:53:06 -0600

    cambio de ruta de archivos

--- insertsql.php
+++ insertsql.php
@@ -7,6 +7,7 @@ $icono = $_POST('icono');
 $autor = $_POST('anio');
 $num_preguntas = $_POST('num_preguntas');
 $ultima_actualiza = $_POST('ultima_actualiza');
+print 'llego:'. $titulo .'descripcion:  '. $descripcion;
 
 require 'conexion.php';
 
