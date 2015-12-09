e962d38b6308459fdc9e78910f987f2256d809e7
Sonia Martinez <soniamartinezctr@gmail.com>
2015-12-09 17:04:52 -0600

    corrección de datos de inserción

--- insertsql.php
+++ insertsql.php
@@ -1,13 +1,13 @@
 <?php
 //$id = $_POST('id');
-$titulo = $_POST('titulo');
-$descripcion = $_POST('descripcion');
-$tabla = $_POST('tabla');
-$icono = $_POST('icono');
-$autor = $_POST('anio');
-$num_preguntas = $_POST('num_preguntas');
-$ultima_actualiza = $_POST('ultima_actualiza');
-print 'llego:'. $titulo .'descripcion:  '. $descripcion;
+$titulo = $_POST['titulo'];
+$descripcion = $_POST['descripcion'];
+$tabla = $_POST['tabla'];
+$icono = $_POST['icono'];
+$autor = $_POST['anio'];
+$num_preguntas = $_POST['num_preguntas'];
+$ultima_actualiza = $_POST['ultima_actualiza'];
+//print 'llego:'. $titulo .'descripcion:  '. $descripcion;
 
 require 'conexion.php';
 
@@ -37,3 +37,4 @@ require 'conexion.php';
 
 
 $conn->close();
+parent::redirect("index.php");
