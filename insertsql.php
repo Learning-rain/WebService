00a8d2bf7cf2ac4a033fa9b56b9c426586abc9cd
Sonia Martinez <soniamartinezctr@gmail.com>
2015-12-09 17:30:34 -0600

    agregar campo autor

--- insertsql.php
+++ insertsql.php
@@ -1,9 +1,10 @@
 <?php
-$id = $_POST['id'];
+//$id = $_POST['id'];
 $titulo = $_POST['titulo'];
 $descripcion = $_POST['descripcion'];
 $tabla = $_POST['tabla'];
 $icono = $_POST['icono'];
+$autor = $_POST['autor'];
 $autor = $_POST['anio'];
 $num_preguntas = $_POST['num_preguntas'];
 $ultima_actualiza = $_POST['ultima_actualiza'];
@@ -15,10 +16,8 @@ require 'conexion.php';
 //$result = $conn->query($sql);
 //print_r($result);
 
-
     $sql = "INSERT INTO test
-            (`id`,
-            `titulo`,
+            (`titulo`,
             `descripcion`,
             `tabla`,
             `icono`,
@@ -27,7 +26,7 @@ require 'conexion.php';
             `num_preguntas`,
             `ultima_actualiza`)
             VALUES
-            ('$id', '$titulo', '$descripcion', '$tabla', '$icono', '$autor', '$num_preguntas', '$ultima_actualiza');";
+            ('$titulo', '$descripcion', '$tabla', '$icono', '$autor', '$num_preguntas', '$ultima_actualiza');";
 
     if ($conn->query($sql) === TRUE) {
         echo "Registro Agregado";
