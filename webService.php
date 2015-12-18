86576b17af0db0a42944a2e8ce6b31637cb80e80
Sonia Martinez <soniamartinezctr@gmail.com>
2015-12-18 13:24:35 -0600

    inicio de programación del WebService

---
+++ webService.php
@@ -0,0 +1,20 @@
+<?php
+$id = $_GET['id'];
+
+require 'conexion.php';
+
+
+$sql = "SELECT * FROM test WHERE id = ".$id;
+
+if ($conn->query($sql) === TRUE) {
+    echo "Registro Agregado";
+    $consulta = "SELECT * FROM test;";
+    $resultado = $conn->query($consulta);    
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+$conn->close();
+foreach ($resultado as $value)
+   {
+   print_r($value);
+   }
