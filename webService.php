5f1f1cbf3752b7e0aabe9ca151037844fb1a0174
Sonia Martinez <soniamartinezctr@gmail.com>
2015-12-18 13:41:53 -0600

    MODIFICACIÓN EN QUERY

--- webService.php
+++ webService.php
@@ -3,18 +3,15 @@ $id = $_GET['id'];
 
 require 'conexion.php';
 
-
-$sql = "SELECT * FROM test WHERE id = ".$id;
-
 if ($conn->query($sql) === TRUE) {
     echo "Registro Agregado";
-    $consulta = "SELECT * FROM test;";
+    $consulta = "SELECT * FROM test WHERE id=".$id;
     $resultado = $conn->query($consulta);    
+        foreach ($resultado as $value)
+           {
+           echo $value;
+           }    
 } else {
     echo "Error: " . $sql . "<br>" . $conn->error;
 }
 $conn->close();
-foreach ($resultado as $value)
-   {
-   echo $value;
-   }
