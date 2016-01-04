52cdfcdb9ead510f2041f9e17b71387d11e15ba2
Sonia Martinez <soniamartinezctr@gmail.com>
2016-01-04 17:18:13 -0600

    Se cambio el password de la conexión

--- conexion.php
+++ conexion.php
@@ -1,8 +1,8 @@
 <?php
 $servername = "localhost";
 $username = "root";
-$password = "amazon5web1";
-$dbname = "bd_learning";
+$password = "k6dM2SS1BJc1";
+$dbname = "learning_bd";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
